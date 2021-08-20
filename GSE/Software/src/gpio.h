3956320227bad313ca433c95b836583802987bb8
John Doty <jpd@noqsi.com>
2021-08-20 17:18:37 -0400

    GSE stuff

---
+++ GSE/Software/src/gpio.h
@@ -0,0 +1,127 @@
+// GPIO bit numbers
+
+#define FORCE0	0
+#define FORCE1	2
+#define EVENT_RDY	3
+#define TEST_ENABLE	5
+
+// SPIO CE bits
+
+#define ADR 	0
+#define	DATA	1
+
+// SPI1 CE bit
+
+#define EVENT	0
+
+// spiFlags consists of the least significant 22 bits.
+// 
+// 21 20 19 18 17 16 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+//  b  b  b  b  b  b  R  T  n  n  n  n  W  A u2 u1 u0 p2 p1 p0  m  m
+// 
+// 
+// mm defines the SPI mode.
+// 
+// Warning: modes 1 and 3 do not appear to work on the auxiliary SPI.
+// 
+// Mode POL PHA
+//  0    0   0
+//  1    0   1
+//  2    1   0
+//  3    1   1
+// 
+// 
+// px is 0 if CEx is active low (default) and 1 for active high.
+// 
+// ux is 0 if the CEx GPIO is reserved for SPI (default) and 1 otherwise.
+// 
+// A is 0 for the main SPI, 1 for the auxiliary SPI.
+// 
+// W is 0 if the device is not 3-wire, 1 if the device is 3-wire. Main SPI only.
+// 
+// nnnn defines the number of bytes (0-15) to write before switching the MOSI line to MISO to read data. This field is ignored if W is not set. Main SPI only.
+// 
+// T is 1 if the least significant bit is transmitted on MOSI first, the default (0) shifts the most significant bit out first. Auxiliary SPI only.
+// 
+// R is 1 if the least significant bit is received on MISO first, the default (0) receives the most significant bit first. Auxiliary SPI only.
+// 
+// bbbbbb defines the word size in bits (0-32). The default (0) sets 8 bits per word. Auxiliary SPI only.
+// 
+
+// ADR and DATA register flags
+
+//  b  b  b  b  b  b  R  T  n  n  n  n  W  A u2 u1 u0 p2 p1 p0  m  m
+//  0  0  0  0  0  0  0  0  0  0  0  0  0  0  1  0  0  0  0  0  0  0
+
+#define ADRFLAGS 	0b10000000
+#define DATAFLAGS	0b10000000
+
+// ADC flags
+
+//  b  b  b  b  b  b  R  T  n  n  n  n  W  A u2 u1 u0 p2 p1 p0  m  m
+//  0  0  0  0  0  0  0  0  0  0  0  0  0  1  1  1  0  0  0  0  0  0
+
+#define EVENTFLAGS	0b111000000
+
+// ADR R/W Description
+// x00 rw   null (no write)
+#define NULLADR 0x00
+// x01 rw   MODE
+#define MODE 0x01
+//           [    0] 0=off, 1=photons
+// x02 rw   HV_CTRL
+#define HV_CTRL 0x02
+//           [    0] 0=off, 1=on
+// x03 rw   DAC_DATA
+#define DAC_DATA 0x03
+//           [15: 0] AD5328 DAC data
+// x10 rw   FPM0_FAST_ADC_GAIN
+#define FPM0_FAST_ADC_GAIN 0x10
+//           [ 2: 0] 2^N ADC gain
+// x11 rw   FPM0_FAST_ADC_OFFSET
+#define FPM0_FAST_ADC_OFFSET 0x11
+//           [15: 0] 16-bit ADC offset
+// x12 rw   FPM0_SLOW_ADC_GAIN
+#define FPM0_SLOW_ADC_GAIN 0x12
+// x13 rw   FPM0_SLOW_ADC_OFFSET
+#define FPM0_SLOW_ADC_OFFSET 0x13
+// x20 rw   FPM1_FAST_ADC_GAIN
+#define FPM1_FAST_ADC_GAIN 0x20
+// x21 rw   FPM1_FAST_ADC_OFFSET
+#define FPM1_FAST_ADC_OFFSET 0x21
+// x22 rw   FPM1_SLOW_ADC_GAIN
+#define FPM1_SLOW_ADC_GAIN 0x22
+// x23 rw   FPM1_SLOW_ADC_OFFSET
+#define FPM1_SLOW_ADC_OFFSET 0x23
+// x30 r    HK_0_0 (FLLD-0)
+#define HK_0_0 0x30
+// x31 r    HK_0_1 (SLLD-0)
+#define HK_0_1 0x31
+// x32 r    HK_0_2 (TEMP-0)
+#define HK_0_2 0x32
+// x33 r    HK_0_3 (HOT-0)
+#define HK_0_3 0x33
+// x34 r    HK_0_4 (HVHK-0)
+#define HK_0_4 0x34
+// x35 r    HK_0_5 (TIS-0)
+#define HK_0_5 0x35
+// x36 r    HK_0_6 (TVS-0)
+#define HK_0_6 0x36
+// x37 r    HK_0_7 (unused)
+#define HK_0_7 0x37
+// x38 r    HK_1_0 (FLLD-1)
+#define HK_1_0 0x38
+// x39 r    HK_1_1 (SLLD-1)
+#define HK_1_1 0x39
+// x3A r    HK_1_2 (TEMP-1)
+#define HK_1_2 0x3A
+// x3B r    HK_1_3 (HOT-1)
+#define HK_1_3 0x3B
+// x3C r    HK_1_4 (HVHK-1)
+#define HK_1_4 0x3C
+// x3D r    HK_1_5 (TIS-1)
+#define HK_1_5 0x3D
+// x3E r    HK_1_6 (TVS-1)
+#define HK_1_6 0x3E
+// x3F r    HK_1_7 (unused)
+#define HK_1_7 0x3F
---
+++ GSE/Software/src/lexigse.c
@@ -0,0 +1,254 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <pigpio.h>
+#include <pthread.h>
+#include <time.h>
+
+#include "gpio.h"
+#include "hkcal.h"
+
+void gpio_perror(const char *s, int n);
+
+static void mode( unsigned gpio, unsigned mode ){
+	int code = gpioSetMode( gpio, mode );
+	if( code >= 0 ) return;
+	fprintf( stderr, "GPIO %u ", gpio );
+	gpio_perror( "lexigse", code );
+	exit( 1 );
+}
+
+static void input( unsigned gpio ){
+	mode( gpio, PI_INPUT );
+}
+
+static void zero( unsigned gpio ){
+	mode( gpio, PI_OUTPUT );
+	int code = gpioWrite( gpio, 0 );
+	if( code >= 0 ) return;
+	fprintf( stderr, "GPIO %u ", gpio );
+	gpio_perror( "lexigse", code );
+	exit( 1 );
+}
+
+// Beware that when exit() is called, pigpio may be in an uninitialized
+// state, so cleanup() must reinitialize it in order to use it.
+// There's no error checking here because there's no safe and sane response.
+
+static void cleanup( void ){
+	gpioInitialise();	// make sure we can turn HV off
+	zero( TEST_ENABLE );	// disable the interface
+	gpioTerminate();
+}
+
+static void timetag( void ){
+	int seconds, micros;
+	gpioTime(PI_TIME_ABSOLUTE, &seconds, &micros);
+	printf( "%f\t", seconds + .0000001*micros );
+}
+
+static pthread_mutex_t m_capture;	// Lock for event capture
+
+
+static void lock_capture( void ) {
+	int code = pthread_mutex_lock( &m_capture );
+	if( code ) {
+		perror( "lexigse" );
+		exit( 1 );
+	}
+}
+
+static void unlock_capture( void ) {
+	int code = pthread_mutex_unlock( &m_capture );
+	if( code ) {
+		perror( "lexigse" );
+		exit( 1 );
+	}
+}
+
+static pthread_mutex_t m_out;		// Lock for output stream
+
+
+static void lock_out( void ) {
+	int code = pthread_mutex_lock( &m_out );
+	if( code ) {
+		perror( "lexigse" );
+		exit( 1 );
+	}
+}
+
+static void unlock_out( void ) {
+        int code = fflush( stdout );
+	if( code ) {
+		perror( "fflush" );
+		exit( 1 );
+	}
+	code = pthread_mutex_unlock( &m_out );
+	if( code ) {
+		perror( "lexigse" );
+		exit( 1 );
+	}
+}
+
+static void log_time( void ){
+	lock_out();
+	timetag();
+	time_t now = time(NULL);
+	printf( "time\t%s", ctime(&now));
+	unlock_out();
+}
+
+// static int click_id;
+// 
+// static void init_click( uint32_t us, int cycles ) {
+// 	gpioPulse_t pulse[2];
+// 	int code;
+// 	
+// 	pulse[0].gpioOn = 1 << CLICK;
+// 	pulse[0].gpioOff = 0;
+// 	pulse[0].usDelay = us;
+// 	pulse[1].gpioOn = 0;
+// 	pulse[1].gpioOff = 1 << CLICK;
+// 	pulse[1].usDelay = us;
+// 	
+// 	while( cycles-- ) {
+// 		code = gpioWaveAddGeneric( 2, pulse );
+// 		if( code < 0 ) {
+// 			gpio_perror( "lexigse", code );
+// 			exit( 1 );
+// 		}
+// 	}
+// 	click_id = gpioWaveCreate();
+// 	if( click_id < 0 ) {
+// 		gpio_perror( "lexigse", code );
+// 		exit( 1 );
+// 	}
+// }
+// 
+static int force_id[2];
+
+static void init_force( void ) {
+	int i;
+	
+	for( i = 0; i < 2; i += 1 ) {
+		gpioPulse_t pulse[2];
+		int code;
+		int bit;
+		if( i ) bit = FORCE1;
+		else bit = FORCE0;
+		
+		pulse[0].gpioOn = 1 << bit;
+		pulse[0].gpioOff = 0;
+		pulse[0].usDelay = 1;		// DUMP is a 1 us pulse
+		pulse[1].gpioOn = 0;
+		pulse[1].gpioOff = 1 << DUMP;
+		pulse[1].usDelay = 0;
+		code = gpioWaveAddGeneric( 2, pulse );
+		if( code < 0 ) {
+			gpio_perror( "lexigse", code );
+			exit( 1 );
+		}
+		force_id[ i ] = gpioWaveCreate();
+		if( force_id[ i ] < 0 ) {
+			gpio_perror( "lexigse", code );
+			exit( 1 );
+		}
+	}
+}
+
+static int adr_reg, data_reg, event_reg;
+
+static void initialize( void ){
+	
+	int code = gpioInitialise();
+	if( code < 0 ) {
+		gpio_perror( "lexigse", code );
+		exit( 1 );
+	}
+	
+	atexit( cleanup );
+	
+	zero( TEST_ENABLE );
+	zero( FORCE0 );
+	zero( FORCE1 );
+	input( EVENT_READY );
+	
+	adr_reg = spiOpen( ADR, 1000000, ADRFLAGS );
+	if( adr_reg ) {
+		gpio_perror( "lexigse", adr_reg );
+		exit( 1 );
+	}
+	
+	data_reg = spiOpen( DATA, 1000000, DATAFLAGS );
+	if( data_reg ) {
+		gpio_perror( "lexigse", data_reg );
+		exit( 1 );
+	}
+	
+	event_reg = spiOpen( EVENT, 1000000, EVENTFLAGS );
+	if( event_reg ) {
+		gpio_perror( "lexigse", event_reg );
+		exit( 1 );
+	}
+	
+	code = pthread_mutex_init( &m_out, NULL );
+	if( code ) {
+		perror( "lexigse" );
+		exit( 1 );
+	}
+	
+//	init_click( 100, 20 );
+	
+	code = gpioSetTimerFunc( TIME_TIMER, 60000, log_time );
+	if( code ) {
+		gpio_perror( "lexigse", code );
+		exit( 1 );
+	}
+	
+	log_time();
+}
+
+
+// static void click( void ) {
+// 	int code = gpioWaveTxSend( click_id, PI_WAVE_MODE_ONE_SHOT );
+// 	if( code < 0 ) gpio_perror( "lexigse", code );
+// }
+
+static void loop( void ){
+	unsigned channel, adcval, i;
+	
+	for(;;){
+		errno = 0;	/* so that EOF is distinguished from error */
+		char *line = NULL;
+		size_t ll = 0;
+		int code = getline( &line, &ll, stdin );
+		if (code == -1 ) {
+			if(errno != 0) { 
+                           perror("lexigse");
+                           exit( 1 );
+                        }
+			return;
+		}
+		lock_out();
+		timetag();
+		printf( "command\t%s", line );
+		unlock_out();
+		if( strncmp( "click", line, 5 ) == 0 ) {
+//			click();
+		} else {
+			fprintf(stderr, 
+				"unrecognized lexigse command: %s\n", line );
+			exit( 1 );
+		}
+		
+		free( line );
+	}
+}
+		
+
+int main( int argc, char **argv ) {
+	initialize();
+	loop();
+	exit( 0 );
+}
